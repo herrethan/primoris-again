f27270ca1adbd169f5a714e1423d55a93a1a60a6
Ethan Herr <eherr@reonomy.com>
2017-11-28 23:16:04 -0500

    add back admin

---
+++ plugins/pico_edit/config.php
@@ -0,0 +1,11 @@
+<?php
+
+global $backend_password;
+
+/*
+ * This should be a sha1 hash of your password.
+ * Use a tool like http://www.sha1-online.com to generate.
+ */
+$backend_password = '3e7f76c6ceb31cf2e677753c7a67efb34147eec6';
+
+?>
