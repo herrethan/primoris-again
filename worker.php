45c2c676cd75417ae71542a5a58d284718e1bdd0
Ethan Herr <eherr@reonomy.com>
2018-08-18 09:10:48 -0400

    see if aws is available for job

--- worker.php
+++ worker.php
@@ -1,5 +1,26 @@
 <?php
 
-print "Dingy time: " . time();
 
+require 'vendor/autoload.php';
+
+$s3_client = Aws\S3\S3Client::factory(array(
+    'credentials' => array(
+        'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
+        'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+    )
+));
+
+$pages_path = $_ENV['AWS_BUCKET_PREFIX'] . $_ENV['AWS_PAGES_DIR'];
+
+$iterator = $s3_client->getIterator('ListObjects', array(
+    'Bucket' => $_ENV['AWS_BUCKET_NAME'],
+    'Prefix' => $pages_path,
+));
+
+foreach ($iterator as $object) {
+    $file = substr($object['Key'], strlen($pages_path), -strlen('.md');
+    if($file && $file != '404'){
+        print($file);
+    }
+}
 ?>
