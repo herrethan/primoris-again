a4aa22d14c1ee7a6d673b5ad88caaa630eec78f5
Ethan Herr <eherr@reonomy.com>
2018-08-18 18:04:53 -0400

    test

--- worker.php
+++ worker.php
@@ -1,27 +1,27 @@
 <?php
 
-print $_ENV['AWS_PAGES_DIR'];
-// require 'vendor/autoload.php';
 
-// $s3_client = Aws\S3\S3Client::factory(array(
-//     'credentials' => array(
-//         'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
-//         'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
-//     )
-// ));
+require 'vendor/autoload.php';
 
-// $pages_path = $_ENV['AWS_BUCKET_PREFIX'] . $_ENV['AWS_PAGES_DIR'];
+$s3_client = Aws\S3\S3Client::factory(array(
+    'credentials' => array(
+        'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
+        'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+    )
+));
 
-// $iterator = $s3_client->getIterator('ListObjects', array(
-//     'Bucket' => $_ENV['AWS_BUCKET_NAME'],
-//     'Prefix' => $pages_path,
-// ));
+$pages_path = $_ENV['AWS_BUCKET_PREFIX'] . $_ENV['AWS_PAGES_DIR'];
 
-// foreach ($iterator as $object) {
-//     $file = substr($object['Key'], strlen($pages_path), -strlen('.md'));
-//     if($file && $file != '404'){
-//         print($file);
-//     }
-// }
+$iterator = $s3_client->getIterator('ListObjects', array(
+    'Bucket' => $_ENV['AWS_BUCKET_NAME'],
+    'Prefix' => $pages_path,
+));
+
+foreach ($iterator as $object) {
+    $file = substr($object['Key'], strlen($pages_path), -strlen('.md'));
+    if($file && $file != '404'){
+        print($file);
+    }
+}
 
 ?>
